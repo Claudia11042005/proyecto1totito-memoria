3b3a4310c2b0e528e3bc5e51cb74b206f33e2de6
Claudia11042005 <claudiatax05@gmail.com>
2024-10-26 13:45:06 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Funcionalidad básica de los botones
+document.getElementById('jurgoTotito').addEventListener('click', function() {
+    alert('¡Listo para jugar Jurgo Totito!');
+});
+
+document.getElementById('juego-Memoria').addEventListener('click', function() {
+    alert('¡A prepararse para Juego Memoria!');
+});
+
+document.getElementById('exit').addEventListener('click', function() {
+    alert('Has salido del juego.');
+});
+
